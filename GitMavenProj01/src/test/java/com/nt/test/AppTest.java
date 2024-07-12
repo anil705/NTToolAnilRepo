0ae27f3ddcf018036bb06c8cd8b74382fbdf20ee
HULK <HULK@HULK>
2024-07-12 17:24:36 +0530

    Adding more test

--- GitMavenProj01/src/test/java/com/nt/test/AppTest.java
+++ GitMavenProj01/src/test/java/com/nt/test/AppTest.java
@@ -15,4 +15,26 @@ public class AppTest {
 		int actual=ar.sum(20, 20);
 		assertEquals(expected, actual);
 	}
+	@Test
+	public void testwithNegatives() {
+		Arithmetic ar=new Arithmetic();
+		int expected=-40;
+		int actual=ar.sum(-20, -20);
+		assertEquals(expected, actual);
+	}
+	@Test
+	public void testwithZeros() {
+		Arithmetic ar=new Arithmetic();
+		int expected=0;
+		int actual=ar.sum(0, 0);
+		assertEquals(expected, actual);
+	}
 }
+
+
+
+
+
+
+
+
